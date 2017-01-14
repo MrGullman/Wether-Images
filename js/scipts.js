d6af3a70b3ac541cbc427675b8d8b06f7c6645d5
MrGullman <jesper.gullman@outlook.com>
2017-01-14 02:40:54 +0100

    Fixat Img Size
    kollar så att bilderna är liggande.

--- js/scipts.js
+++ js/scipts.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
   */
   
   $.getJSON("https://ipapi.co/json/", function(data){
-      console.log(data);
+      //console.log(data);
       city = "stockholm";
       //city = data.city;
       getCity(city);
@@ -54,7 +54,7 @@ $(document).ready(function(){
       
     /*$.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=98607e305c6ce579f817cf36c9aaa852&lang=se", function(data)*/
     $.get("https://api.apixu.com/v1/current.json?key=e5c6bcdb884f449e97904419171101&q=" + city +"", function(data){
-      console.log(data);
+      //console.log(data);
       //var icon = data.weather[0].icon; // få ut väder icon värde
       //var temp = data.main.temp-273.15; // omvandla kelvin till celcius
       var weatherType = data.current.condition.text;
@@ -62,11 +62,11 @@ $(document).ready(function(){
       $('.country').text(data.location.country);
       //$('.temp').text(temp.toFixed(1));
       $('.temp').text(data.current.temp_c);
-      console.log(data.current.condition.text);
+      //console.log(data.current.condition.text);
       $('.description').text(data.current.condition.text);
       var icon = data.current.condition.code;
       var dayOrNight = data.current.is_day;
-      console.log(data.current.condition.code);
+      //console.log(data.current.condition.code);
       //$('.icon').append("<img src=" + icon + ">");
       getIcon(icon);
 
@@ -88,21 +88,53 @@ $(document).ready(function(){
     
     function getImages(weather){
       
-        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9bb1ef3285f869282ecc5e8ff9526ad5&per_page=30&tags=" + weather + "&extras=url_c%2C+url_m%2C+url_q"
+        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9bb1ef3285f869282ecc5e8ff9526ad5&per_page=100&tags=" + weather + "&extras=url_c%2C+url_m%2C+url_q"
+        
         var weather = weather;
 
         $.getJSON(url + "&format=json&jsoncallback=?", function(data){
+
+          
+          var imageArray = [];
           console.log(data);
+          // check image size
+          for(var i = 0; i < data.photos.photo.length; i++){
+            for(var i in data.photos.photo){
+              console.log(data.photos.photo[i].width_m)
+              
+              if(data.photos.photo[i].width_m === "500"){
+                imageArray.push(data.photos.photo[i].url_m)
+              }
+            }       
+          }
+          
+          console.dir(imageArray); 
+          
           var photoHTML = '<ul>';
-          $.each(data.photos.photo, function(i, photo){
+          
+          $.each(imageArray, function(i, photo){
             photoHTML += '<li>';
-            photoHTML += '<img src="' + photo.url_m + '"></li>';
+            
+            photoHTML += '<img src="' + photo + '"></li>';
           });
             photoHTML += '</ul>';
           $('#photos').html(photoHTML);
           
-          
-          console.log(data.photos);
+          /*$.each(data.photos.photo, function(i, photo){
+            photoHTML += '<li>';
+            
+            photoHTML += '<img src="' + photo.url_m + '"></li>';
+          });
+            photoHTML += '</ul>';
+          $('#photos').html(photoHTML);
+          */
+          /*for(var i = 0; i < data.photos.photo.length; i++){
+            for(var j = 0; j < data.photos.photo[i]; j++){
+              console.log([j]);
+            }
+            
+          }*/
+
         });
       
     };
@@ -112,8 +144,14 @@ $(document).ready(function(){
   
   //--------------------------------------------------------------
   
+  //  Check image size 
   
   
+  
+  
+  
+  
+  //--------------------------------------------------------------
   
   //--------------------------------Capital City-----------------
   
@@ -146,7 +184,7 @@ $(document).ready(function(){
     var capitalC = $('#item').val();
     getCity(capitalC);
     getImages(capitalC);
-    console.log(capitalC);
+    //console.log(capitalC);
   });
   
   
